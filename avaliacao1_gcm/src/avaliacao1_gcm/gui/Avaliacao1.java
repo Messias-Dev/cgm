7e9182800186e1f29c7220df1d0022f4da2283cd
Messias Nunesmaia <messias.lsn@gmail.com>
2021-10-25 20:58:53 -0300

    Initial commit

---
+++ avaliacao1_gcm/src/avaliacao1_gcm/gui/Avaliacao1.java
@@ -0,0 +1,20 @@
+package avaliacao1_gcm.gui;
+
+import java.util.Scanner;
+
+public class Avaliacao1 {
+
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Digite CPF: ");
+		String cpf = sc.next();
+		sc.close();
+
+		// TODO validar CPF e imprimir msg
+		
+
+	}
+
+}
